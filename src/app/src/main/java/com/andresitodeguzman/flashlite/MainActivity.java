f7fe18cf4e1dea16ff5febfeef4b313be226df60
Andresito de Guzman <andresitomyemail@gmail.com>
2018-09-14 13:37:41 +0800

    Added android files

---
+++ src/app/src/main/java/com/andresitodeguzman/flashlite/MainActivity.java
@@ -0,0 +1,16 @@
+package com.andresitodeguzman.flashlite;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		
+		Button switchButton = findViewById(R.id.
+    }
+}
